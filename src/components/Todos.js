eff2304132275481898f597fd7802c57ec40068b
sujitoic999 <sujitfgiet@gmail.com>
2024-03-10 14:14:09 +0530

    'trying to implement update feature'

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { removeTodo } from "../features/todo/todoSlice";
+import { updateTodo } from "../features/todo/todoSlice";
 
 function Todos() {
   const todos = useSelector((state) => state.todos);
@@ -11,7 +12,9 @@ function Todos() {
       <div>Todos</div>
       {todos.map((todo) => (
         <li key={todo.id}>
-          {todo.text}
+          <span onDoubleClickCapture={() => dispatch(updateTodo(todo.id))}>
+            {todo.text}
+          </span>
           <button onClick={() => dispatch(removeTodo(todo.id))}>X</button>
         </li>
       ))}
--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -18,10 +18,12 @@ export const todoSlice = createSlice({
     removeTodo: (state, action) => {
       state.todos = state.todos.filter((todo) => todo.id !== action.payload);
     },
-    updateTodo: (state, action) => {},
+    updateTodo: (state, action) => {
+      console.log(action.payload);
+    },
   },
 });
 
-export const { addTodo, removeTodo } = todoSlice.actions;
+export const { addTodo, removeTodo, updateTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
